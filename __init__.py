057a6cafb5b57ff6093a34a98c3deaf240691fa1
XSky123 <dlxiaotian01@gmail.com>
2017-10-21 14:36:42 +0800

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+----------------------------------------
+    Filename   : __init__.py
+    Description: 
+    Author     : XSky123
+    Created at : 2017/10/21 14:35
+----------------------------------------
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
