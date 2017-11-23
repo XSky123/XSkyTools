258cc2d85c18b2291882ff731f5a288eca0d5eb9
XSky123 <dlxiaotian01@gmail.com>
2017-11-23 17:35:17 +0800

    Make Logger as a class.
    Origin method also be kept.

--- Log.py
+++ Log.py
@@ -16,6 +16,34 @@ import logging
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 _logger = logging.getLogger(__name__)
 
+class Logger:
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+
+    def set_debug_mode(self):
+        self._logger.setLevel(logging.DEBUG)
+        self._logger.debug("Logger now set to debug mode.")
+
+    def set_info_mode(self):
+        self._logger.setLevel(logging.INFO)
+        self._logger.debug("Logger now set to info mode.")
+
+    def set_default_mode(self):
+        self._logger.setLevel(logging.WARNING)
+        self._logger.debug("Logger now set to default mode.")
+
+    def debug(self, content):
+        self._logger.debug(content)
+
+    def info(self, content):
+        self._logger.info(content)
+
+    def warning(self, content):
+        self._logger.warning(content)
+
+    def error(self, content):
+        self._logger.error(content)
+
 
 def set_debug_mode():
     _logger.setLevel(logging.DEBUG)
@@ -49,9 +77,10 @@ def error(content):
 
 
 def main():
-    set_debug_mode()
-    info("This is an info.")
-    warning("Warn!!")
+    log = Logger()
+    log.set_debug_mode()
+    log.info("This is an info.")
+    log.warning("Warn!!")
 
 if __name__ == '__main__':
     main()
