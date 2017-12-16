7606e3463fcb61b6afbb5f9cf17dc3f79a87195b
XSky123 <dlxiaotian01@gmail.com>
2017-12-16 15:20:22 +0800

    add time tool.

---
+++ Time.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+----------------------------------------
+    Filename   : Time.py
+    Description: Deal with time
+    Author     : XSky123
+    Created at : 2017/12/16 15:17
+----------------------------------------
+"""
+import datetime
+
+
+def str_now():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
